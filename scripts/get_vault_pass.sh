86a98f7433a967b4a1c37a9eaba4b4df7f22311f
Bhupinder Singh Gill <bhupinderhappy777@gmail.com>
2026-02-27 03:47:50 +0000

    zsh added to common

--- scripts/get_vault_pass.sh
+++ scripts/get_vault_pass.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+OCI_BIN="/home/bhupi/bin/oci"
+OCID=$(grep "ansible_vault_secret_ocid" ~/.oci/config | cut -d'=' -f2 | xargs)
 
-# Check if the OCID is set as an environment variable
-if [ -z "$ANSIBLE_SECRET_OCID" ]; then
-    echo "Error: ANSIBLE_SECRET_OCID is not set." >&2
+if [ -z "$OCID" ]; then
+    echo "Error: OCID not found in ~/.oci/config" >&2
     exit 1
 fi
 
-# Fetch the secret using the environment variable
-oci secrets secret-bundle get --secret-id "$ANSIBLE_SECRET_OCID" \
+$OCI_BIN secrets secret-bundle get --secret-id "$OCID" \
   --query "data.\"secret-bundle-content\".content" --raw-output | base64 --decode
